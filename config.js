81e5406f2d9d80fb26ea7638d019ff007e11e864
snafill <89634992+snafill@users.noreply.github.com>
2022-08-15 10:13:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default {
   title: 'yun yu yuan', // 网站标题
   nickName: "云与原", // 昵称
   githubName: 'yunyuyuan', // github账户名
-  githubRepo: 'nuxt2-blog', // 勿修改
+  githubRepo: 'snafill', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
   SEO_description: '云与原的博客，书写值得书写的东西',
